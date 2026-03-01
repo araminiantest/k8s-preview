795a00f27ca23adf9e8affd967dbc81bd39b8d3e
araminiantest <aminian.rmin@gmail.com>
2026-03-01 13:35:39 +0100

    Update Heading.jsx

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -2,7 +2,7 @@ function Heading() {
     return (
         <div className="heading">
             <h1>Todo List</h1>
-            <h2>Welcome to Preview Environment! 🚀</h2>
+            <h2>Welcome to Preview Environment!! 🚀</h2>
         </div>
     )
 }
